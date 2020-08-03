2f784b97dceda7fd1ebe855c6f5ded128dd319a1
sevkioruc34@gmail.com <sevkioruc34@gmail.com>
2020-08-03 23:10:58 +0300

    App vue clear

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
-  <button @click="run">Run</button>
 </template>
 
 <script>
-import noti from './components/notification/index';
-import notification from './components/notification/notifications'
 export default {
-  components: {
-    notification
-  },
-  methods: {
-    run() {
-      let params = {};
-      params.message = 'FromApp1';
-      params.state = 'warning';
-      params.position = 'bottom-left';
-
-      noti(params);
-    }
-  }
 }
 </script>
-
-<style>
-
-</style>
